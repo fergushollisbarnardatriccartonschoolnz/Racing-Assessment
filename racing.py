34fb9b96d002e1df5086336a27532622683bb0d1
fergushollisbarnardatriccartonschoolnz <fergushollisbarnard@riccarton.school.nz>
2025-05-06 09:13:22 +1200

    added player

--- racing.py
+++ racing.py
@@ -13,10 +13,13 @@ clock = pygame.time.Clock()
 
 ticks = 0
 score = 0
-player_speed = 0
+player_speed = 1
 player_x = 0
+player_y = screen_height*0.75
 player_momentum = 0
 drag = 0.1
+player_x_size = 50
+player_y_size = 50
 
 lanes = 4
 lanesep = 100
@@ -25,9 +28,6 @@ lanewidth = 10
 entities = []
 
 def drawbg(col:tuple, lanecol:tuple):
-    wallpos = (lanes/2)*lanesep+lanesep/2+lanewidth/2
-    print(wallpos)
-    wallwidth = 100
     screen.fill(col)
     pygame.draw.rect(screen, (123, 123, 123), (-wallpos-wallwidth+screen_width/2, 0, wallwidth, screen_height)) #left wall
     pygame.draw.rect(screen, (123, 123, 123), (wallpos+screen_width/2, 0, wallwidth, screen_height)) #right wall
@@ -42,14 +42,49 @@ def entitytick():
         entity["y"] -= 1
         pygame.draw.rect(screen, entity["col"], (entity["x"], entity["y"], entity["xsc"], entity["ysc"]))
 
+mov = [0, 0]
+keys_left = [pygame.K_a, pygame.K_LEFT]
+keys_right = [pygame.K_d, pygame.K_RIGHT]
 running_program = True
 while running_program:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running_program = False
+        if event.type == pygame.KEYDOWN:
+            if event.key in keys_left:
+                mov[0] = player_speed
+            if event.key in keys_right:
+                mov[1] = player_speed
+        if event.type == pygame.KEYUP:
+            if event.key in keys_left:
+                mov[0] = 0
+            if event.key in keys_right:
+                mov[1] = 0
+
+    wallpos = (lanes/2)*lanesep+lanesep/2+lanewidth/2
+    wallwidth = 100
 
     drawbg((143, 143, 143), (101, 101, 101))
     #(123, 123, 123)
+
+    #basically playertick
+    player_momentum += (-mov[0] + mov[1])
+    player_x += player_momentum
+    if player_x > wallpos-player_x_size/2:
+        player_momentum = -abs(player_momentum)
+        player_x += player_momentum
+    elif player_x < -wallpos+player_x_size/2:
+        player_momentum = abs(player_momentum)
+        player_x += player_momentum
+    if player_momentum < drag and player_momentum > -drag:
+        player_momentum = 0
+    if player_momentum > drag:
+        player_momentum -= drag
+        player_momentum *= 1-drag
+    if player_momentum < -drag:
+        player_momentum += drag
+        player_momentum *= 1-drag
+    pygame.draw.rect(screen, (255, 200, 255), (screen_width/2+player_x-player_x_size/2, player_y-player_y_size, player_x_size, player_y_size))
 
     entitytick()
 
